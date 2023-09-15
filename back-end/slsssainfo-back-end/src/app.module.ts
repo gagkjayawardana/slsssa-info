ebc0cb3dfe803f574fd7d5a41dc26d8f16b7feb1
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2023-09-15 19:10:45 +0530

    Create user entitty using TypeORM entitty

--- back-end/slsssainfo-back-end/src/app.module.ts
+++ back-end/slsssainfo-back-end/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { User } from './user/entities/user.entity';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
